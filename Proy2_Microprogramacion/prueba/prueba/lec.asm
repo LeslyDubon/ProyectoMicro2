bbcabfb9889dbaab71c42d15b07993f9e954b01c
LeslyDubon <35675890+LeslyDubon@users.noreply.github.com>
2019-11-23 03:23:49 -0600

    Agregando documentación interna

---
+++ Proy2_Microprogramacion/prueba/prueba/lec.asm
@@ -0,0 +1,224 @@
+.386
+.model flat, stdcall
+option casemap:none
+
+include ReadFile.inc
+include \masm32\include\masm32rt.inc
+
+.data
+;definicion de datos
+;---Dirección de archivo de lectura
+FileName byte "test2.txt",0
+;---Cadenas de impresión
+ingreso db "Ingrese la palabra a buscar: ",0
+noencontrado db "La palabra no se encuentra en el archivo. ",0
+encontrado db "La palabra fue ingresada el ",0
+;---Bandera de encontrado
+bEncontrado db 0
+;---Contador de fecha
+fechan db 19
+
+.data?
+;---Cadena
+buffer db 100 dup(?)
+;---Variables temporales de la palabra y el archivo
+temp dd ?
+temp2 dd ?
+;---Handler
+hFile dd ?
+;---Tamaño de archivo
+Filesize dd ?
+;---Buffer de Lectura
+hMemory dd ?
+BytesRead dd ?
+
+.code
+start:
+
+	main proc
+		;---Ingreso de la palabra de búsqueda
+		invoke StdOut, addr ingreso
+		invoke StdIn,addr buffer,100
+
+		;---Creación del handler para leer el archivo
+		invoke CreateFile,addr FileName,GENERIC_READ OR GENERIC_WRITE,FILE_SHARE_READ OR FILE_SHARE_WRITE, NULL,OPEN_ALWAYS,FILE_ATTRIBUTE_NORMAL,NULL
+		mov hFile,eax
+		cmp hFile, INVALID_HANDLE_VALUE
+		jz code1
+
+		;---Determinar el tamaño del archivo
+		invoke GetFileSize,eax,0
+		mov Filesize, eax
+		inc eax
+		
+		invoke GlobalAlloc,GMEM_FIXED,eax
+		mov hMemory, eax
+
+		add eax, Filesize
+		mov BYTE PTR [eax],0
+
+		;---Lectura del Archivo
+		invoke ReadFile,hFile,hMemory,Filesize,ADDR BytesRead,0
+		invoke CloseHandle,hFile
+
+		call Buscar
+
+		invoke GlobalFree,hMemory
+
+		code1:
+		invoke ExitProcess,0
+		ret
+	main endp
+
+	Buscar proc
+	    XOR esi, esi
+		XOR edi, edi
+
+		;---Mover a punteros		
+		MOV edi, hMemory
+		LEA esi, buffer
+
+		;---Obteniendo primer caracter de la palabra y el archivo
+		MOVZX eax, BYTE PTR [edi]
+		MOV temp2, eax
+		MOVZX ebx, BYTE PTR [esi]
+		MOV temp, ebx
+
+		;---Convierte a mayúsculas
+		call EBXmayuscula
+		call TEMP2mayuscula
+		;---Compara si son iguales o diferentes
+		CMP ebx, temp2
+		JE iguales
+		JNE diferentes
+
+		diferentes:
+		;---Se obtiene el siquiente caracter del archivo y se realiza de nuevo la comparación
+		INC edi
+		MOVZX eax, BYTE PTR [edi]
+		CMP eax, 0
+		JE evaluar
+		call EAXmayuscula
+		call TEMPmayuscula
+		CMP eax, temp
+		JE iguales
+		JNE diferentes
+
+		iguales:
+		;---Incrementa los punteros
+		INC esi
+		INC edi
+		;---Se obtiene el siquiente caracter del archivo y de la palabra. Se realiza de nuevo la comparación
+		MOVZX ebx, BYTE PTR [esi]
+		MOV temp, ebx
+		MOV ebx, temp
+		CMP ebx, 0
+		JE encontrada
+		XOR eax, eax
+		MOVZX eax, BYTE PTR [edi]
+		MOV temp2, eax
+		XOR eax, eax
+		MOV eax, temp2
+		call EAXmayuscula
+		call TEMPmayuscula
+		CMP temp, eax
+		JE iguales
+		JNE volverComenzar
+
+		volverComenzar:
+		;---Vuelve a iniciar el puntero de palabra y compara
+		XOR esi, esi
+		LEA esi, buffer
+		MOVZX ebx, BYTE PTR [esi]
+		MOV temp, ebx
+		JMP diferentes
+
+		evaluar:
+		;---Evaluación de al menos 1 resultado
+		CMP bEncontrado,0
+		JE noencontrada
+		JMP Fin
+
+		noencontrada:
+		;---Impresión de la cadena no encontrada
+		invoke StdOut, addr noencontrado
+		JMP Fin
+
+		encontrada:
+		;---Impresión de la cadena encontrada junto a la fecha y vuelve a comenzar
+		invoke StdOut, addr encontrado
+		INC bEncontrado
+		call imprimirFecha
+		print " ",10,13
+		mov edx, 20d
+		mov fechan, dl
+		JMP volverComenzar
+
+		Fin:
+		ret
+
+	Buscar endp
+
+	imprimirFecha proc
+		XOR EAX, EAX
+		;---se repite hasta encontrar la fecha
+		espacio:
+		INC edi
+		MOVZX eax, BYTE PTR [edi]
+		CMP eax, 32
+		JNE espacio
+	
+		;---Impresión de la fecha
+		fecha:
+		INC edi
+		MOVZX eax, BYTE PTR [edi]
+		push eax
+		print esp
+		pop eax
+		dec fechan
+		jnz fecha
+		ret
+	imprimirFecha endp
+
+	;---Funciones de conversión a mayúsuculas
+	EAXmayuscula proc
+	CMP AX,61H
+	JB mayuscula
+	CMP AX,7AH
+	JA mayuscula
+	AND AX,11011111B
+	mayuscula:
+	ret
+	EAXmayuscula endp
+
+	EBXmayuscula proc
+	CMP BX,61H
+	JB mayuscula
+	CMP BX,7AH
+	JA mayuscula
+	AND BX,11011111B
+	mayuscula:
+	ret
+	EBXmayuscula endp
+
+	TEMPmayuscula proc
+	CMP temp,61H
+	JB mayuscula
+	CMP temp,7AH
+	JA mayuscula
+	AND temp,11011111B
+	mayuscula:
+	ret
+	TEMPmayuscula endp
+
+	TEMP2mayuscula proc
+	CMP temp2,61H
+	JB mayuscula
+	CMP temp2,7AH
+	JA mayuscula
+	AND temp2,11011111B
+	mayuscula:
+	ret
+	TEMP2mayuscula endp
+
+end start
---
+++ Proy2_Microprogramacion/prueba/prueba/pruebapr.asm
@@ -0,0 +1,145 @@
+.386
+.model flat, stdcall
+
+option casemap:none 
+include \masm32\include\windows.inc 
+include \masm32\include\user32.inc 
+include \masm32\include\kernel32.inc 
+include \masm32\include\masm32.inc
+include \masm32\include\masm32rt.inc
+
+includelib \masm32\lib\user32.lib
+includelib \masm32\lib\kernel32.lib
+includelib \masm32\lib\masm32.lib
+
+
+.DATA
+;---Definición de la ruta del archivo de escritura
+FileName db "test2.txt",NULL
+;---Mensajes de éxito y error de la creación del archivo
+BadText db "Its not ok",0
+OkText db "Its ok",0
+;---Mensaje de impresión de acciones
+format db 'You pressed %s',10,0
+WriteText  db "This is some test text.",0
+thechar db 0, 0
+;---Formatos de fecha y hora
+formatofecha DB " dd-MM-yyyy",0
+formatohora DB " hh:mm:ss",0
+espacio DB " ",0
+
+
+.DATA?
+;---Handle
+hFile HANDLE ?
+;---Buffers de fecha y hora
+fechaBuf DB 50 DUP(?)
+horaBuf DB 50 DUP(?)
+BytesRead dd ?
+BytesWritten dd ?
+
+.CODE
+start1:
+	sl: 
+	;---Creación del archivo
+    invoke CreateFile,addr FileName,GENERIC_READ OR GENERIC_WRITE,FILE_SHARE_READ OR FILE_SHARE_WRITE, NULL,OPEN_ALWAYS,FILE_ATTRIBUTE_NORMAL,NULL
+	mov hFile,eax
+	cmp hFile, INVALID_HANDLE_VALUE
+    jz code1
+
+	;---Espera a que una tecla sea presionada
+	call wait_key
+	mov thechar, al
+	;---Compara el char con enter
+	cmp thechar, 13
+	je fecha1
+	;---Compara el char con el espacio
+	cmp thechar, 32
+	je fecha1
+	cmp thechar, 0E0h
+	je start2
+	invoke SetFilePointer,hFile,0,0,FILE_END
+	mov BytesRead, 1
+	mov BytesWritten, 1
+	invoke WriteFile, hFile, addr thechar, BytesRead, addr BytesWritten,  0
+	invoke CloseHandle, hFile
+    jmp sl
+
+fecha1:
+	;---Formato de fecha y hora
+	INVOKE GetDateFormat , 0 , 0 , \
+	0 , ADDR formatofecha , ADDR fechaBuf, 50
+	MOV EBX, OFFSET fechaBuf
+	INVOKE GetTimeFormat , 0 , 0 , \
+	0 , ADDR formatohora , ADDR horaBuf , 50
+	;---Imprime un enter
+	mov thechar, 10
+	invoke SetFilePointer,hFile,0,0,FILE_END
+	mov BytesRead, 11
+	mov BytesWritten, 11
+	;---Escribe la fecha y hora
+	invoke WriteFile, hFile, addr fechaBuf, BytesRead, addr BytesWritten,  0
+	invoke WriteFile, hFile, addr horaBuf, BytesRead, addr BytesWritten,  0
+	mov BytesRead, 1
+	mov BytesWritten, 1
+	invoke WriteFile, hFile, addr thechar, BytesRead, addr BytesWritten,  0
+	invoke CloseHandle, hFile
+	jmp sl
+
+fecha2:
+	;---Formato de fecha y hora
+	INVOKE GetDateFormat , 0 , 0 , \
+	0 , ADDR formatofecha , ADDR fechaBuf, 50
+	MOV EBX, OFFSET fechaBuf
+	INVOKE GetTimeFormat , 0 , 0 , \
+	0 , ADDR formatohora , ADDR horaBuf , 50
+	;---Imprime un enter
+	mov thechar, 10
+	invoke SetFilePointer,hFile,0,0,FILE_END
+	mov BytesRead, 11
+	mov BytesWritten, 11
+	;---Escribe la fecha y hora
+	invoke WriteFile, hFile, addr fechaBuf, BytesRead, addr BytesWritten,  0
+	invoke WriteFile, hFile, addr horaBuf, BytesRead, addr BytesWritten,  0
+	mov BytesRead, 1
+	mov BytesWritten, 1
+	invoke WriteFile, hFile, addr thechar, BytesRead, addr BytesWritten,  0
+	invoke CloseHandle, hFile
+	jmp start2
+
+code1:
+    invoke MessageBox,NULL,addr BadText,addr BadText,MB_OK
+    invoke ExitProcess,0
+    ret
+
+start2: 
+	;---Creación del archivo
+    invoke CreateFile,addr FileName,GENERIC_READ OR GENERIC_WRITE,FILE_SHARE_READ OR FILE_SHARE_WRITE, NULL,OPEN_ALWAYS,FILE_ATTRIBUTE_NORMAL,NULL
+    mov hFile,eax
+    cmp hFile, INVALID_HANDLE_VALUE
+    jz code1
+
+	;---Espera a que una tecla sea presionada
+	call wait_key
+	mov thechar, al
+
+	;---Compara el char con enter
+	cmp thechar, 13
+	je fecha2
+	;---Compara el char con el espacio
+	cmp thechar, 32
+	je fecha2
+	cmp thechar, 0E0h
+	je sl
+
+	;---Imprime las acciones del teclado
+	invoke crt_printf, ADDR format, ADDR thechar
+	invoke SetFilePointer,hFile,0,0,FILE_END
+	mov BytesRead, 1
+	mov BytesWritten, 1
+	invoke WriteFile, hFile, addr thechar, BytesRead, addr BytesWritten,  0
+	invoke CloseHandle, hFile
+    jmp start2
+
+end start1
+
